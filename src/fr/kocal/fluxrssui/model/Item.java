b9831ea26c67e6e65697f738e8f2ed9661ac65f8
ALLIAUME Hugo <kocal@live.fr>
2016-01-03 22:20:55 +0100

    Renommage de la propriété Item::link en Item::url

--- src/fr/kocal/fluxrssui/model/Item.java
+++ src/fr/kocal/fluxrssui/model/Item.java
@@ -2,14 +2,15 @@ package fr.kocal.fluxrssui.model;
 
 /**
  * Représente un &lt;item&gt; dans flux RSS
+ *
  * @author Hugo Alliaume
  */
 public class Item {
 
     /**
-     * Lien hypertexte de l'item
+     * Adresse URL de l'item
      */
-    private String link;
+    private String url;
 
     /**
      * Titre de l'item
@@ -23,31 +24,34 @@ public class Item {
 
     /**
      * Création d'un objet Item
-     * @param link Lien hypertexte de l'item
-     * @param title Titre de l'item
+     *
+     * @param url         Adresse URL de l'item
+     * @param title       Titre de l'item
      * @param description Description de l'item
      */
-    public Item(String link, String title, String description) {
-        this.link = link;
+    public Item(String url, String title, String description) {
+        this.url = url;
         this.title = title;
         this.description = description;
     }
 
     /**
      * Création d'un objet Item sans description
-     * @param link Lien hypertexte de l'item
+     *
+     * @param url   Adresse URL de l'item
      * @param title Titre de l'item
      */
-    public Item(String link, String title) {
-        this(link, title, "");
+    public Item(String url, String title) {
+        this(url, title, "");
     }
 
     /**
      * Création d'un objet Item sans titre et sans description
-     * @param link Lien hypertexte de l'item
+     *
+     * @param url Adresse URL de l'item
      */
-    public Item(String link) {
-        this(link, "", "");
+    public Item(String url) {
+        this(url, "", "");
     }
 
     /**
@@ -58,23 +62,26 @@ public class Item {
     }
 
     /**
-     * Retourne le lien hypertexte de l'item
-     * @return un lien hypertexte
+     * Retourne l'adresse URL de l'item
+     *
+     * @return une adresse URL
      */
-    public String getLink() {
-        return link;
+    public String getUrl() {
+        return url;
     }
 
     /**
-     * Assigne un lien hypertexte à l'item
-     * @param link Lien hypertexte à assigner
+     * Assigne une adresse URL à l'item
+     *
+     * @param url Adresse URL à assigner
      */
-    public void setLink(String link) {
-        this.link = link;
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     /**
      * Retourne le titre de l'item
+     *
      * @return un titre
      */
     public String getTitle() {
@@ -83,6 +90,7 @@ public class Item {
 
     /**
      * Assigne un titre à l'item
+     *
      * @param title Titre à assigner à l'item
      */
     public void setTitle(String title) {
@@ -91,6 +99,7 @@ public class Item {
 
     /**
      * Retourne la description de l'item
+     *
      * @return une description
      */
     public String getDescription() {
@@ -99,6 +108,7 @@ public class Item {
 
     /**
      * Assigne une description à l'item
+     *
      * @param description Description à assigner
      */
     public void setDescription(String description) {
